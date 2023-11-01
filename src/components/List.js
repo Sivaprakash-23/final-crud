fd58039d791564c40d44793ac0e3fe25f6dbd716
Sivaprakash-23 <prakashmcse2019@gmail.com>
2023-11-01 10:31:58 +0530

    first-commit

---
+++ src/components/List.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import { BsTrash } from 'react-icons/bs';
+import { BiEditAlt } from 'react-icons/bi'; // Change 'BiEditAlt' to 'bi'
+import { baseURL } from '../utils/constant';
+import axios from "axios";
+
+const List = ({ id, task, setUpdateUI, updateMode }) => {
+ const removeTask =() => {
+        axios.delete(`${baseURL}/delete/${id}`).then((res)=>{
+            console.log(res);
+            setUpdateUI((prevState)=> !prevState);
+        });
+    };
+  return (
+    <li>
+      {task}
+      <div className="icon_holder">
+        <BiEditAlt  className='icon' onClick={() =>updateMode(id,task)} />
+        <BsTrash className='icon' onClick={removeTask} />
+      </div>
+    </li>
+  );
+};
+
+export default List;
